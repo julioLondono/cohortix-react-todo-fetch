62d214f6bad5351fc2843e4fd9232558a60368d8
Julio Londono <juliolondonogarcia@gmail.com>
2019-05-24 23:27:36 +0000

    started adding the fetch function

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -41,6 +41,30 @@ export class Home extends React.Component {
 		this.setState(orderedArray);
 	};
 
+	componetDidMount() {
+		fetch("http://assets.breatheco.de/apis/fake/todos/user/alesanchezr", {
+			method: "PUT",
+			body: JSON.stringify(todos),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // will be tru if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+	}
+
 	render() {
 		const mappedItems = this.state.todos.map((item, index) => {
 			let firstLine = index === 0 ? "hideBtn" : "fas fa-angle-up";
